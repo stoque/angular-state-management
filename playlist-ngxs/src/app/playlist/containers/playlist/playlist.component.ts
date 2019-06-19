9a5aae9fb4eda91057e58ddb7c6c271c24cba75d
Lucas Stoque <stoque@live.com>
2019-06-18 21:23:25 -0300

    feat(playlist-ngxs): add components

---
+++ playlist-ngxs/src/app/playlist/containers/playlist/playlist.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-playlist',
+  template: `
+    <p>
+      playlist works!
+    </p>
+  `
+})
+export class PlaylistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- playlist-ngxs/src/app/playlist/playlist.module.ts
+++ playlist-ngxs/src/app/playlist/playlist.module.ts
@@ -1,6 +1,19 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PlaylistComponent } from './containers/playlist/playlist.component';
+import { SongsListComponent } from './components/song-list/songs-list.component';
 
 @NgModule({
-
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    PlaylistComponent,
+    SongsListComponent
+  ],
+  exports: [
+    PlaylistComponent
+  ]
 })
-export class PlayListModule { }
+export class PlaylistModule { }
